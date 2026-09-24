f608cef47287285a9c44a45d7efd73cc9df7e0ca
jasonm23 <jasonm23@gmail.com>
2026-09-24 11:20:01 +0700

    fix base

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
-  base: ".",
+  base: "./",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
