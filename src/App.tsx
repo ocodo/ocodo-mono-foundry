d804436ddbc886c9c715d37eaf95f31d27d43b24
jasonm23 <jasonm23@gmail.com>
2026-09-24 11:57:59 +0700

    fix base for router

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,19 @@ import Index from "@/pages/Index";
 import MonoSpecimen from "@/pages/MonoSpecimen";
 import DotZeroSpecimen from "@/pages/DotZeroSpecimen";
 
+const basename = window.location.pathname.split("/").filter(Boolean)[0]
+  ? `/${window.location.pathname.split("/").filter(Boolean)[0]}`
+  : "/";
+
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <Routes>
         <Route path="/" element={<Index />} />
-
         <Route
           path="/specimen-mono"
           element={<MonoSpecimen />}
         />
-
         <Route
           path="/specimen-mono-dotzero"
           element={<DotZeroSpecimen />}
